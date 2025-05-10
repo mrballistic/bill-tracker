6ccd76dacb520f814f8f0cc35e2fe4e15133c00d
Todd Greco <mrballistic@gmail.com>
2025-05-10 12:02:49 -0700

    fixed bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* ...existing tags... */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="manifest" href="/site.webmanifest" />
