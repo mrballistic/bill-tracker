74c3e7983d3f88b3a57d0b20799b89d61ed44598
Todd Greco <mrballistic@gmail.com>
2025-05-10 12:02:37 -0700

    more branding updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ...existing tags... */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeRegistry>
           <BillProvider>
